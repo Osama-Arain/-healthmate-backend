e8bfacd12876a0ab17a915849053cc5ac91ccd0e
Osama-Arain <osaamaarain@gmail.com>
2025-10-19 05:17:43 +0500

    feat : add cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
